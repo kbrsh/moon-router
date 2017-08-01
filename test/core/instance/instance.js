7bc0f2ac247deb3442e3b50087844575aa0269a4
Kabir Shah <simplecooldude1@gmail.com>
2017-07-31 19:49:14 -0700

    add more tests

--- test/core/instance/instance.js
+++
@@ -1,7 +0,0 @@
-describe("Instance", function() {
-  describe('Initializing', function() {
-    it('with new', function() {
-      expect(new MoonRouter({default: "/", map: {"/": "Root"}}) instanceof MoonRouter).to.equal(true);
-    });
-  });
-});
--- test/core/route/route.js
+++ test/core/route/route.js
@@ -1,46 +1,67 @@
 describe("Route", function() {
-  describe("Initialize", function() {
-    var el = createTestElement("initialize", "<router-view></router-view>");
-    var component = null;
+  var el = createTestElement("route", "<router-link to='/test' class='router-link-class'></router-link><router-view></router-view>");
+  var component = null;
 
-    Moon.component("Root", {
-      template: "<h1>Root Route {{msg}}</h1>",
-      data: function() {
-        return {
-          msg: "Message"
-        }
-      },
-      hooks: {
-        mounted: function() {
-          component = this;
-        }
+  Moon.component("Root", {
+    template: "<h1>Root Route {{msg}}</h1>",
+    data: function() {
+      return {
+        msg: "Message"
       }
-    });
-
-    var router = new MoonRouter({
-      default: "/",
-      map: {
-        "/": "Root"
+    },
+    hooks: {
+      mounted: function() {
+        component = this;
       }
-    });
+    }
+  });
 
-    var app = new Moon({
-      el: "#initialize",
-      router: router
-    });
+  Moon.component("Test", {
+    template: "<h1>Test Route</h1>"
+  });
 
-    it("should initialize a router view", function() {
-      return wait(function() {
-        expect(el.firstChild.nodeName).to.equal("H1");
-        expect(el.firstChild.innerHTML).to.equal("Root Route Message");
-      });
-    });
+  var router = new MoonRouter({
+    default: "/",
+    map: {
+      "/": "Root",
+      "/test": "Test"
+    }
+  });
 
-    it("should update with data", function() {
-      component.set("msg", "Changed");
-      return wait(function() {
-        expect(el.firstChild.innerHTML).to.equal("Root Route Changed");
-      });
+  var app = new Moon({
+    el: "#route",
+    router: router
+  });
+
+  it("should initialize a router view", function() {
+    return wait(function() {
+      expect(el.firstChild.nextSibling.nodeName).to.equal("H1");
+      expect(el.firstChild.nextSibling.innerHTML).to.equal("Root Route Message");
+    });
+  });
+
+  it("should update with data", function() {
+    component.set("msg", "Changed");
+    return wait(function() {
+      expect(el.firstChild.nextSibling.innerHTML).to.equal("Root Route Changed");
+    });
+  });
+
+  it("should navigate with router link", function() {
+    expect(el.firstChild.getAttribute("class")).to.equal("router-link-class");
+    el.firstChild.click();
+    return wait(function() {
+      expect(el.firstChild.nextSibling.innerHTML).to.equal("Test Route");
+      expect(el.firstChild.getAttribute("class")).to.equal("router-link-class router-link-active");
+    });
+  });
+
+  it("should navigate from code", function() {
+    expect(el.firstChild.getAttribute("class")).to.equal("router-link-class router-link-active");
+    router.navigate("/");
+    return wait(function() {
+      expect(el.firstChild.nextSibling.innerHTML).to.equal("Root Route Message");
+      expect(el.firstChild.getAttribute("class")).to.equal("router-link-class");
     });
   });
 });
